8046187b601f2aea25107223b686d5dc7f600c08
Red Pill <israelpeled@gmail.com>
2023-02-17 01:14:56 +0000

    Fix

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# # using the pyTelegramBotAPI
+# using the pyTelegramBotAPI
 
+# for token
 import os
+
+# for chatbot 
 import telebot
 
+# import telegram token from secrets
 bot = telebot.TeleBot(os.environ['TOKEN'])
+
+print('Hello Mendi')
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+  bot.reply_to(message, "Howdy, how are you doing?")
 
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#   bot.reply_to(message, message.text)
+
+
+# create two buttons for the bot
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+def echo_all_buttons(message):
+  bot.reply_to(message, "I'm a bot, and I can do this:")
 
+
+print("Bot Live!")
 bot.polling()
